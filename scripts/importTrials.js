60608c76f0d3245a7e18b15e344e1d318ff41ba5
crisst <criss.t@northeastern.edu>
2026-06-22 16:55:13 -0400

    Scripts importTrials.js
    In this commit, I added the script file to import the trials.json to Railway deployment.

---
+++ scripts/importTrials.js
@@ -0,0 +1,37 @@
+import fs from "fs";
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri);
+
+async function importTrials() {
+  try {
+    await client.connect();
+
+    const db = client.db("clinicalTrials");
+
+    const trials =
+      JSON.parse(
+        fs.readFileSync(
+          "./frontend/data/trials.json",
+          "utf8"
+        )
+      );
+
+    const collection =
+      db.collection("trials");
+
+    await collection.deleteMany({});
+
+    await collection.insertMany(trials);
+
+    console.log(
+      `Imported ${trials.length} records`
+    );
+  } finally {
+    await client.close();
+  }
+}
+
+importTrials();
